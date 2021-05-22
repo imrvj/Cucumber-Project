a1d20ff180a5a6871065c840cc18d8d60061c183
RV <sng.rv07@gmail.com>
2021-05-22 17:21:47 +0530

    changes

--- src/test/java/Cucumber_Learn/stepDefinition/StepDefinition.java
+++ src/test/java/Cucumber_Learn/stepDefinition/StepDefinition.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -15,10 +16,15 @@ public class StepDefinition  {
 
 	public  WebDriver driver;
 	 
+	@Before
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Ranvijay\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+	}
     @Given("^user is on homepage$")
     public void user_is_on_homepage() throws Throwable {     
-    	System.setProperty("webdriver.chrome.driver","C:\\Users\\Ranvijay\\Downloads\\chromedriver.exe");
-        driver = new ChromeDriver();
+    	
     	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://automationpractice.com/index.php");
     }
